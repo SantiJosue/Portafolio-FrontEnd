86049d023c84bf58dd90156c904ba6bc077c1b4f
Santiago <santicomercio7@gmail.com>
2023-03-22 20:59:57 -0300

    moviendo carpeta front

---
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
+
+@Component({
+  selector: 'app-experiencia',
+  templateUrl: './experiencia.component.html',
+  styleUrls: ['./experiencia.component.css']
+})
+export class ExperienciaComponent implements OnInit {
+  expe: Experiencia[] = [];
+
+  constructor(private sExperienciaService: SExperienciaService, private tokenService: TokenService) { }
+
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.cargarExperiencia();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }
+    else{
+      this.isLogged = false;
+    }
+  }
+
+    cargarExperiencia(): void{
+      this.sExperienciaService.lista().subscribe(
+        data => {
+          this.expe = data
+        })
+  }
+
+
+
+  delete(id?: number){
+    if(id != undefined){
+      this.sExperienciaService.delete(id).subscribe(
+        data => {
+          this.cargarExperiencia();
+        }, err =>{
+          alert("No se pudo borrar la experiencia");
+        }
+      )
+    }
+  }
+
+}
